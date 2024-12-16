0df7c2dd4485022fc34043de95cf6484afc7a750
ahab8055 <muhammadahab8055@gmail.com>
2024-12-16 13:09:01 +0500

    View Page design added

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,28 @@
 import { createLogger, createStore } from "vuex";
-
+import instance from "@/config/instance";
 
 const store = createStore({
   plugins: [createLogger()],
   state: {
-    users: []
+    pokemons: []
+  },
+  getters: {},
+  actions: {
+    async fetchPokemons({ commit }) {
+      try {
+        const { data } = await instance.get("pokemon?limit=9");
+        commit("SET_POKEMONS", data.results);
+      } catch (error) {
+        console.log(error);
+      }
+    }
   },
   mutations: {
-    setUsers(state, users) {
-      state.users = users;
+    SET_POKEMONS(state, pokemons) {
+      state.pokemons = pokemons;
     }
   },
   
 })
+
+export default store;
